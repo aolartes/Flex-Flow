d14efa17191a0a2e27ad88200d7ef2b9caef1a55
Alvaro Olarte <alvaroolartes@hotmail.com>
2024-02-20 22:05:22 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const numeros = (a) =>{
+    const arreglo = [];
+    for(i=1;i<=a;i++){        
+        arreglo.push(i);
+    }
+    return arreglo;
+}
+
+const resultadoMostrar = numeros(10);
+
+console.log("alvar");
+
+
+// document.querySelector('.prue').innerHTML = 'El resultado es: '+resultadoMostrar;
+
+const contenedoresResultados = document.querySelectorAll('.prue, .prue-row-nowrap, .prue-row-wrap, .prue-wrap-reverse, .prue-reverse-wrap-reverse')
+
+for (var i = 0;i<resultadoMostrar.length;i++){
+    console.log(resultadoMostrar[i]);
+}
+
+
+contenedoresResultados.forEach((contenedor) => {
+    resultadoMostrar.forEach((resultado)=> {
+        const divResultado =  document.createElement("div");
+        divResultado.textContent= `${resultado}`;
+        divResultado.classList.add("numers");        
+        contenedor.appendChild(divResultado);
+    });    
+});
